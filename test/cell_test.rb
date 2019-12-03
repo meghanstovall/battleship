ba722f4cae2beec5cdf3b8feda92114d7f506028
Alex Lynch <mlynch5187@gmail.com>
2019-12-03 15:45:27 -0700

    Update cell test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -20,6 +20,24 @@ class CellTest < Minitest::Test
     assert_equal true, cell.empty?
   end
 
-  
+  def test_place_ship
+    cell = Cell.new("B4")
+    ship = Ship.new("Cruiser", 3)
+    cell.place_ship(ship)
+
+    assert_equal ship, cell.ship
+    assert_equal false, cell.empty?
+  end
+
+  def test_ship_is_fired_upon
+    cell = Cell.new("B4")
+    ship = Ship.new("Cruiser", 3)
+    cell.place_ship(ship)
+
+    assert_equal false, cell.fired_upon?
+    cell.fired_upon
+    assert_equal 2, cell.ship.health
+    assert_equal true, cell.fired_upon
+  end
 
 end
