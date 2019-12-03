20ab0c9f6a4fa11a2dba7a9a1a7da3db301ead26
Alex Lynch <mlynch5187@gmail.com>
2019-12-03 15:30:44 -0700

    Create cell test on new branch

---
+++ test/cell_test.rb
@@ -0,0 +1,25 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+
+  def test_it_exists
+    cell = Cell.new("B4")
+
+    assert_instance_of Cell, cell
+  end
+
+  def test_it_has_attributes
+    cell = Cell.new("B4")
+
+    assert_equal "B4", cell.coordinate
+    assert_equal nil, cell.ship
+    assert_equal true, cell.empty?
+  end
+
+  
+
+end
