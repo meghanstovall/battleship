98ec2b5e8605c07bf08b8d8367ddce801e7f29bf
Meghan Stovall <meghan.stovall@gmail.com>
2019-12-03 12:22:43 -0700

    Create ship class, and first tests for that class.

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,8 +1,50 @@
+
+# pry(main)> cruiser.health
+# #=> 3
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 2
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 1
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.sunk?
+# #=> true
+
+
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/ship'
 
-class ShipTest
+class ShipTest < Minitest::Test
+
+  def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+  end
+
+  def test_attributes_exist_when_created
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+    assert_equal 3, cruiser.health
+    assert_equal false, cruiser.sunk?
+  end
+
 
 end
