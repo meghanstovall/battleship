dc6c9a557ed45f074878681501ce19bb3e2c6fcb
Alex Lynch <mlynch5187@gmail.com>
2019-12-05 12:57:20 -0700

    Update board test and board class

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,4 +1,19 @@
 gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
 require './lib/board'
 
 class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new("Board")
+
+    assert_instance_of "Board", board.cells
+  end
+
+
+
+
+end
