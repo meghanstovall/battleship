cb0f3e4dcd9adb4dfeec31f91ca56a4ec1ac56c4
Alex Lynch <mlynch5187@gmail.com>
2019-12-05 13:06:35 -0700

    Made changes per partner's request deleted .class from board test setup

--- test/board_test.rb
+++ test/board_test.rb
@@ -10,7 +10,7 @@ class BoardTest < Minitest::Test
   def test_it_exists
     board = Board.new("Board")
 
-    assert_instance_of "Board", board.cells
+    assert_instance_of "Board", board
   end
 
 
